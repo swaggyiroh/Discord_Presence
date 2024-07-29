3e27082dddb87a790b3b21eb48b4789d16f97fae
swaggyiroh <swaggyiroh@users.noreply.github.com>
2024-07-29 20:16:52 +0200

    change code

--- discordPresenceAPI.py
+++ discordPresenceAPI.py
@@ -1,8 +1,8 @@
-import discord
-from discord.ext import commands
-from flask import Flask, render_template_string
+from flask import Flask, render_template, request
 from flask_cors import CORS
 from dotenv import load_dotenv
+import discord
+from discord.ext import commands
 import os
 import time
 import requests
@@ -53,7 +53,8 @@ def get_presence(user_id):
     
     latest_activity = None
     latest_start_time = 0
-    activity_logo_url = None
+    activity_info = {}
+    activities_html = "<p>No activity found.</p>"
 
     for guild in bot.guilds:
         member = guild.get_member(user_id)
@@ -68,18 +69,7 @@ def get_presence(user_id):
                     if start_timestamp > latest_start_time:
                         latest_activity = activity
                         latest_start_time = start_timestamp
-                        
-                        # Special handling for Spotify activity
-                        if isinstance(activity, discord.Spotify):
-                            activity_logo_url = activity.album_cover_url
-                        else:
-                            if activity.assets and 'large_image' in activity.assets:
-                                asset_id = activity.assets['large_image'].split(':')[-1]
-                                activity_logo_url = f"https://cdn.discordapp.com/app-assets/{activity.application_id}/{asset_id}.png"
     
-    activities_html = ''
-    main_activitylogo_url = "https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png"
-
     if latest_activity:
         current_time = int(time.time())
         start_time = getattr(latest_activity, 'start', None)
@@ -95,15 +85,15 @@ def get_presence(user_id):
                 "track_id": latest_activity.track_id,
                 "song_elapsed": format_elapsed_time(current_time - latest_activity.start.timestamp()),
                 "song_duration": format_elapsed_time(latest_activity.duration.total_seconds()),
-                "activity_logo_url": activity_logo_url,
+                "activity_logo_url": latest_activity.album_cover_url,
                 "elapsed_time": formatted_elapsed_time,
             }
 
             activities_html = f"""
             <div style="text-align: left;">
-                <p><strong>Song:</strong> {activity_info['song_title']}<br>
-                <strong>Artist:</strong> {activity_info['artist']}<br>
-                <strong>Album:</strong> {activity_info['album']}<br>
+                <p><strong>{activity_info['song_title']}</strong><br>
+                by {activity_info['artist']}<br>
+                on  {activity_info['album']}<br>
                 </p>
             </div>
             """
@@ -113,128 +103,28 @@ def get_presence(user_id):
                 "activity_name": getattr(latest_activity, 'name', "N/A"),
                 "details": getattr(latest_activity, 'details', 'N/A'),
                 "state": getattr(latest_activity, 'state', 'N/A'),
-                "activity_logo_url": activity_logo_url,
+                "activity_logo_url": latest_activity.large_image_url,
                 "elapsed_time": formatted_elapsed_time,
             }
 
-            # Conditionally display details and state if they are not None
-            details_html = f"<br>{activity_info['details']}" if activity_info['details'] != 'N/A' else ""
-            state_html = f"<br>{activity_info['state']}" if activity_info['state'] != 'N/A' else ""
-
+            
+            
             activities_html = f"""
             <div style="text-align: left;">
-                <p>{activity_info['activity_name']}{details_html}{state_html}<br>
+                <p>{activity_info['activity_name']}<br>
+                {activity_info['details'] + '<br>' if activity_info['details'] else ''}
+                {activity_info['state'] + '<br>' if activity_info['state'] else ''}
                 {activity_info['elapsed_time']}
                 </p>
             </div>
-            """
+        """
 
-        main_activitylogo_url = activity_info["activity_logo_url"] if activity_info['activity_logo_url'] else "https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png"
-
-    html_template = f"""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>{user_name}'s Discord Presence</title>
-        <style>
-            body {{
-                background-color: #2c2f33;
-                color: #ffffff;
-                font-family: Arial, sans-serif;
-                text-align: center;
-                padding: 20px;
-                margin: 0;
-            }}
-            .profile {{
-                display: grid; 
-                grid-template-columns: 1.6fr 1.4fr; 
-                gap: 2vh 0px; 
-                grid-template-areas: 
-                    "top top"
-                    "bottom bottom"; 
-                margin: 20px auto;
-                padding: 20px;
-                background-color: #23272a;
-                border-radius: 10px;
-                width: fit-content;
-                align-items: center;
-            }}
-            .top {{ grid-area: top;display: flex; }}
-            .bottom {{ grid-area: bottom; }}
-            .username {{
-                margin-top: 0px;
-                align-items: center;
-            }}
-            .avatar {{
-                text-align: center;
-                margin-right: 20px;
-                transform: scale(1.1);
-                transform-origin: top left;
-            }}
-            .avatar img {{
-                border-radius: 50%;
-                margin-bottom: 10px;
-            }}
-            .banner-container {{
-                display: flex;
-                flex-direction: column;
-                align-items: flex-start;
-            }}
-            .banner {{
-            }}
-            .banner img{{
-                width:300px;
-                height:110px;
-                border-radius: 10px;
-            }}
-            .activity-container {{
-                display: flex;
-                align-items: center;
-                gap:1vw;
-                flex-direction: row;
-            }}
-            .activity-container img {{
-                width:110px;
-                height:110px;
-                border-radius: 20px;
-                }}
-            .activity {{
-                margin-top: -10px;
-            }}
-            .activity-logo{{
-                height: 128px;
-                width: 128px;
-                transform: scale(0.7);
-                background-repeat: no-repeat; /* Prevent repeating the image */
-                border-radius: 20px;
-                transform-origin: left top;
-                background-image: url('{main_activitylogo_url}');
-            }}
-        </style>
-    </head>
-    <body>
-        <div class="profile">
-            <div class="top">
-                <div class="avatar">
-                    <img src="{user_avatar_url}" alt="Avatar" width="100" height="100">
-                    <p class="username">{user_name}</p>
-                </div>
-                <div class="banner-container">
-                    <div class="banner">
-                        <img src="{banner_url}" alt="Banner">
-                    </div>
-                </div>
-            </div>
-            <div class="bottom">
-                {f'<div class="activity-container"><img src="{main_activitylogo_url}" alt="" width="100" height="100"><div class="activity">{activities_html}</div></div>' if activities_html else ''}
-            </div>
-        </div>
-    </body>
-    </html>
-    """
-    
-    return render_template_string(html_template)
+    return render_template('user_presence.html',
+                           user_name=user_name,
+                           user_avatar_url=user_avatar_url,
+                           banner_url=banner_url,
+                           main_activitylogo_url=activity_info.get("activity_logo_url", 'https://via.placeholder.com/100x100?text=No+Activity'),
+                           activities_html=activities_html)
 
 
 def run_bot():
